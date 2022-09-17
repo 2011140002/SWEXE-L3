143b93e4926fb217492f15ad21430a06d5625518
Ubuntu <ubuntu@ip-172-31-71-136.ec2.internal>
2022-09-17 15:58:20 +0000

    Completed 200 OK in 81ms (Views: 44.6ms | ActiveRecord: 10.7ms)

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -26,8 +26,8 @@ class TweetsController < ApplicationController
   
   def update
     tweet = Tweet.update(message: params[:tweet][:message])
+    
     rescue ActiveRecord::RecordInvalid => e
-    pp e.record.errors
     redirect_to '/'
   end
   
