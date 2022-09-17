d6c79099bba501d2bc15b178bc6a2443ea5b0078
Ubuntu <ubuntu@ip-172-31-71-136.ec2.internal>
2022-09-17 14:41:36 +0000

    ArgumentError (Missing :controller key on routes definition, please check your routes.):

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root 'tweets#index'
   get 'tweets/index'
+  get 'tweets/create'
+  delete 'tweets/#{id}'
+  get 'tweets/#{id}', to: "tweets#show"
+  get 'tweets/#{id}/edit' , to: "tweets#edit"
+  patch "tweets/#{id}/update", to: "tweets#update"
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
