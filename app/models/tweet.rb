1e32026fcfeec59c0e252366acbc1ab74fed23c0
Ubuntu <ubuntu@ip-172-31-71-136.ec2.internal>
2022-09-17 17:50:12 +0000

    Completed 200 OK in 53ms (Views: 46.5ms | ActiveRecord: 0.7ms)

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,6 @@
 class Tweet < ApplicationRecord
     validates :message, presence: true
     validates :message, length: { maximum: 140 }
-    mount_uploader :image, ImageUploader
+    #mount_uploader :image, ImageUploader
 end
 
