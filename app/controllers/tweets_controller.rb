8be76cc466a35608b79eae724bbc73f281b5a855
Ubuntu <ubuntu@ip-172-31-71-136.ec2.internal>
2022-09-17 14:38:24 +0000

    ActionView::Template::Error (No route matches {:action=>"new", :controller=>"tweets"}):

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,22 @@
 class TweetsController < ApplicationController
   def index
+    @tweets = Tweet.all
   end
+  
+  def create
+    
+  end
+  
+  def update
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def destroy
+    
+  end
+  
 end
