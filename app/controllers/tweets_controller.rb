ea8dc346057d5afc4bb93173a80fed6f15d76620
Ubuntu <ubuntu@ip-172-31-71-136.ec2.internal>
2022-09-17 15:15:50 +0000

    Completed 200 OK in 67ms (Views: 63.6ms | ActiveRecord: 0.8ms)

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,10 +6,15 @@ class TweetsController < ApplicationController
   def new
     @tweet = Tweet.new
   end
+  
   def create
     tweet  = Tweet.new(message: params[:tweet][:message], tdate: Time.current)
     tweet.save
     redirect_to '/'
+  end
+  
+  def show
+    @tweet = Tweet.find(params[:id])
   end
   
   def edit
