17e3b21f43a1e501fad366454ad4c168354ab81f
Ubuntu <ubuntu@ip-172-31-71-136.ec2.internal>
2022-09-17 14:42:01 +0000

    NameError (undefined local variable or method `id' for #<ActionDispatch::Routing::Mapper:0x00007fdd824aed38>):

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'tweets#index'
   get 'tweets/index'
   get 'tweets/create'
-  delete 'tweets/#{id}'
+  delete 'tweets/#{id}' , to: "tweets#destroy"
   get 'tweets/#{id}', to: "tweets#show"
   get 'tweets/#{id}/edit' , to: "tweets#edit"
   patch "tweets/#{id}/update", to: "tweets#update"
